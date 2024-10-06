b4d5e1f60d134515ad23264f067c43ba35c54853
Jerom Palimattom Tom <83979298+jeromtom@users.noreply.github.com>
2024-10-06 23:22:41 +0530

    hide helpers

--- Scroll Story/Lynx Scroll Story/main.js
+++ Scroll Story/Lynx Scroll Story/main.js
@@ -37,11 +37,11 @@ scene.add(pointLight, ambientLight);
 
 // Helpers
 
-const lightHelper = new THREE.PointLightHelper(pointLight)
-const gridHelper = new THREE.GridHelper(200, 50);
-scene.add(lightHelper, gridHelper)
+// const lightHelper = new THREE.PointLightHelper(pointLight)
+// const gridHelper = new THREE.GridHelper(200, 50);
+// scene.add(lightHelper, gridHelper)
 
-const controls = new OrbitControls(camera, renderer.domElement);
+// const controls = new OrbitControls(camera, renderer.domElement);
 
 function addStar() {
   const geometry = new THREE.SphereGeometry(0.25, 24, 24);
